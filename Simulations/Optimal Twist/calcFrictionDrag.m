2c9a6baf89a5454813ad41018f5ef2507b50f8eb
cram9030 <ncramer@ucsc.edu>
2016-10-10 17:53:51 -0700

    Created a new folder for simulations
    Also added calcFrictionDrag that estimates the friction

---
+++ Simulations/Optimal Twist/calcFrictionDrag.m
@@ -0,0 +1,53 @@
+function Cd = calcFrictionDrag(U)
+
+%Equations from Fundamentals of Airplane Flight Mechanics and Aerodynamics
+%for Engineers
+
+%Body and wing deminsions
+t = 36.576e-3;
+c = 0.3048;
+Sexposed = 2*0.58205878*c;
+crossSecWing = 0.007328002;
+l = 0.4191;
+d = 0.10374122;
+h = 0.127;
+noseSA = 0.0378896836;
+
+%Refrence Area
+S = 1.289685*c;
+
+%Air constants
+airDensity = 1.225; %kg/m^3
+airDynamicViscosity = 12.27e-6; %Pa*s
+airSoundSpeed = 340.29; %m/s
+mach = U/airSoundSpeed;
+
+%Reynolds number for body and wings
+ReBody = airDensity*U*.4191/airDynamicViscosity;
+ReWings = airDensity*U*0.3048/airDynamicViscosity;
+
+%Compressibility factor
+CF_k = (1.0+0.2*mach.^2).^(-0.467);
+
+%Interferance Factor
+Ib = 1.20;
+Iw = 1.20;
+
+%Form Factor
+FFw = 1.0+1.6*(t/c)+100*(t/c)^4;
+FFB = 1.0+60/(l/d)^3+0.0025*(l/d);
+
+%Wetted area
+Swing = 2.0*(1+0.2*t/c)*Sexposed;
+SBody = noseSA-2*crossSecWing+2*l*d+2*h*l;
+
+%Coefficient of Friction - Prandtl-Schlichting formula
+Cfw = 0.455./(log10(ReWings)).^2.58-1700./ReWings;
+Cfb = 0.455./(log10(ReBody)).^2.58-1700./ReBody;
+
+%Force Calculation
+fw = Cfw.*CF_k*Iw*FFw*Swing;
+fb = Cfb.*CF_k*Ib*FFB*SBody;
+
+%CD Calculation
+Cd = 1.1*(fw+fb)/S;
---
+++ createPressureMovie.m
@@ -0,0 +1,45 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [] = createPressureMovie(lattice,results,Q,filename)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% createCPMovie: Function for Dynamic TORNADO						
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This function simulates the dynamic tip twist of the wing for a
+% proscribed tip profile
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%	Author:	Nick Cramer, UCSC, Department of Computer Engineering			
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CONTEXT:	Function for Dynamic TORNADO Aeroelastic Simulator					
+% Called by:	---------FILL IN LATER----------												
+% Calls:	MATLAB 5.2 std fcns
+%
+% Inputs:   lattice - lattice structure
+%           results - array of result structures
+%           filename - name of video file that will be saved
+% Output:   
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[M,N] = size(results);
+
+F(N) = struct('cdata',[],'colormap',[]);
+
+figure(4)% 	Delta cp plot
+rotate3d on 
+colormap(hot);
+fill3(lattice(1).XYZ(:,:,1)',lattice(1).XYZ(:,:,2)',lattice(1).XYZ(:,:,3)',results(1).cp'*Q(1))
+minLattice = min(min([lattice(:).XYZ]));
+maxLattice = max(max([lattice(:).XYZ]));
+axis equal
+axis([minLattice(1) maxLattice(1) minLattice(2) maxLattice(2) minLattice(3) maxLattice(3)])
+caxis([min(min([results(:).cp]'))*min(Q) max(max([results(:).cp]'))*max(Q)])
+title('Pressure Distribution')
+colorbar('vert')
+ax = gca;
+ax.NextPlot = 'replaceChildren';
+
+for j = 1:N
+    fill3(lattice(j).XYZ(:,:,1)',lattice(j).XYZ(:,:,2)',lattice(j).XYZ(:,:,3)',results(j).cp'*Q(j))
+    drawnow
+    F(j) = getframe(gcf);
+end
+
+movie2avi(F,filename,'compression','None')
